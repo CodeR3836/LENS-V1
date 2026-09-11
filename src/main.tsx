eb605be1be6918ab1a92f7a58150902db9861817
CodeR <tasin.abir10941455@gmail.com>
2026-09-11 17:59:30 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import CropApp from "./CropApp";
 
-// Detect whether this window was opened as the crop overlay
 const isCropMode = new URLSearchParams(window.location.search).has("crop");
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
